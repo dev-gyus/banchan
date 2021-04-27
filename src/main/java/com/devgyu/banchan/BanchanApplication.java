af5265876dbb928bf0aa685a22cc44c9439243cc
GyuHyeong <cjsworbehd13@gmail.com>
2021-04-27 16:06:15 +0900

    mariaDB 의존성 추가

--- src/main/java/com/devgyu/banchan/BanchanApplication.java
+++ src/main/java/com/devgyu/banchan/BanchanApplication.java
@@ -5,6 +5,8 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,5 +29,4 @@ public class BanchanApplication {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
 }
